1ccee6a68b9ee56a61ee66cbec78a826a1ad0dfa
alterlleo <leonardo.pasquato@gmail.com>
2026-02-26 16:00:27 +0100

    feat: setup negotiator class

---
+++ negotiator.cpp
@@ -0,0 +1,1 @@
+#include "negotiator.hpp"
---
+++ negotiator.hpp
@@ -0,0 +1,38 @@
+#ifndef __NEGOTIATOR_HPP__
+
+#include <nlohmann/json.hpp>
+
+using namespace std;
+using json = nlohmann::json;
+
+
+class Negotiator{
+
+  public:
+
+    Negotiator();
+    ~Negotiator();
+
+    void set_covariance(double c) { _covariance = c; }
+    double get_covariance();
+    double get_proposed_power();
+
+    void listen();
+    void speak();
+    void update_proposal();
+
+  private:
+
+    double _required_power = 0;
+    double _proposed_power = 0;
+    double _covariance = 0;
+    
+
+    double _residual;
+
+};
+
+
+
+
+#endif
